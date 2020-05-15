efb5b5e58af926c28398c5a9cdeff864e66e1fbb
BenWix <44454993+BenWix@users.noreply.github.com>
2020-05-15 12:00:19 -0400

    removing methods from models

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -6,6 +6,7 @@ class LocationController < ApplicationController
     
     get '/locations/:id' do 
         @location = Location.find_by_id(params[:id])
+        @common_birds = group_by_hash(@location.birds).sort_by{|bird, count| count}.reverse[0..4]
         if @location 
             erb :'locations/show'
         else 
